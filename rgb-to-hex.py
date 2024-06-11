aaa2379cfd4d412729ed0a667f7cc95ba3430407
Emanuel <emarqu@pm.me>
2024-06-11 01:20:29 -0700

    pasted hex to rgb from stack 😬

---
+++ rgb-to-hex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Conversions
+def rgb_to_hex(rgb):
+    print("...")
+
+def hex_to_rgb(hex):
+    h = hex.lstrip('#')
+    print('rgb', tuple(int(h[i:i+2], 16) for i in (0, 2, 4)))
+
+# Read values to convert
+f = open("convert.txt", "r")
+lines = f.readlines()
+
+# Convert read values
+for line in lines:
+    if line[0:3] == 'rgb':
+        rgb_to_hex(line.strip())
+    elif line.strip(): 
+        hex_to_rgb(line.strip())
+
+# Display converted values
