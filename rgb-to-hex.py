8e8b5ff3856de08659e0efe5ad87fd1b2da4abde
Emanuel <emarqu@pm.me>
2024-06-11 12:19:51 -0700

    v1

--- rgb-to-hex.py
+++ rgb-to-hex.py
@@ -2,28 +2,22 @@
 
 # Conversions
 def rgb_to_hex(rgb):
-    res = []
-    x=rgb.split()
-    for i in x:
-        print(i)
-        if i.isnumeric():
-            res.append(int(i))
-    print(res)
-    print('-')
+    res = [int(i) for i in rgb.split() if i.isdigit()]
+    if len(res) == 3: 
+        h = '#%02x%02x%02x' % (res[0], res[1], res[2])
+        print(rgb + ' == ' + h)
 
 def hex_to_rgb(hex):
     h = hex.lstrip('#')
-    print('rgb', tuple(int(h[i:i+2], 16) for i in (0, 2, 4)))
+    print(hex + ' == rgb', tuple(int(h[i:i+2], 16) for i in (0, 2, 4)))
 
 # Read values to convert
 f = open("convert.txt", "r")
 lines = f.readlines()
 
-# Convert read values
+# Convert & display read values
 for line in lines:
     if line[0:3] == 'rgb':
         rgb_to_hex(line.strip())
-    # elif line.strip(): 
-        # hex_to_rgb(line.strip())
-
-# Display converted values
+    elif line[0:1] == '#': 
+        hex_to_rgb(line.strip())
